45ac38312047acad88a949bc9df7ca9318ec7f2d
qepting91 <64446420+qepting91@users.noreply.github.com>
2024-01-29 17:37:05 -0500

    update pdf link

--- src/pages/program.pdf.js
+++
@@ -1,9 +0,0 @@
-// src/pages/datasheet.pdf.js
-export default function handler(req, res) {
-    res.status(200).send({
-      headers: {
-        'Content-Type': 'application/pdf',
-      },
-      body: Buffer.from('/12-weekprogram.pdf', 'base64'),
-    });
-  }
